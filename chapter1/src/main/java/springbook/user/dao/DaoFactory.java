7e2d88be485253549acb8633c6023c0b0048dbd2
mirrors89 <lee01494@gmail.com>
2017-05-02 10:15:31 +0900

    Toby ver.1 [chapter1] 제어의 역전(IoC) : 오브젝트 팩토리 활용법

--- chapter1/src/main/java/springbook/user/dao/DaoFactory.java
+++ chapter1/src/main/java/springbook/user/dao/DaoFactory.java
@@ -3,9 +3,11 @@ package springbook.user.dao;
 public class DaoFactory {
     public UserDao userDao() {
         // 팩토리 메소드는 UserDao 타입의 오브젝트를 어떻게 만들고, 어떻게 준비시킬지를 결정한다.
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        return new UserDao(connectionMaker());
+    }
 
-        return userDao;
+    public ConnectionMaker connectionMaker() {
+        // 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
+        return new DConnectionMaker();
     }
 }
