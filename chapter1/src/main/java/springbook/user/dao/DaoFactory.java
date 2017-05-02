c96c57eb766b238b374d41266f9078970a14dc9a
mirrors89 <lee01494@gmail.com>
2017-05-02 13:29:06 +0900

    Toby ver.1 [chapter1] 스프링의 IoC : 오브젝트 팩토리를 이용한 Spring loC

--- chapter1/src/main/java/springbook/user/dao/DaoFactory.java
+++ chapter1/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,11 +1,17 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         // 팩토리 메소드는 UserDao 타입의 오브젝트를 어떻게 만들고, 어떻게 준비시킬지를 결정한다.
         return new UserDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         // 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
         return new DConnectionMaker();
