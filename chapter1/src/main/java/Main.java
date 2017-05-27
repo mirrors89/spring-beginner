5537ad620f6e67622954dc75df7a3e73f3dda914
mirrors89 <lee01494@gmail.com>
2017-05-27 12:51:11 +0900

    Toby ver.1 [chapter1] XML을 이용한 설정 : XML을 이용하는 애플리케이션 컨텍스트

--- chapter1/src/main/java/Main.java
+++ chapter1/src/main/java/Main.java
@@ -9,24 +9,24 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         // ConnectionMaker connectionMaker = new NConnectionMaker();
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        UserDao dao = new UserDao(connectionMaker);
-
-        User user = new User();
-        user.setId("mirror");
-        user.setName("이기승");
-        user.setPassword("password");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + " 조회 성공");
+//        ConnectionMaker connectionMaker = new DConnectionMaker();
+//
+//        UserDao dao = new UserDao(connectionMaker);
+//
+//        User user = new User();
+//        user.setId("mirror");
+//        user.setName("이기승");
+//        user.setPassword("password");
+//
+//        dao.add(user);
+//
+//        System.out.println(user.getId() + " 등록 성공");
+//
+//
+//        User user2 = dao.get(user.getId());
+//        System.out.println(user2.getName());
+//        System.out.println(user2.getPassword());
+//
+//        System.out.println(user2.getId() + " 조회 성공");
     }
 }
--- chapter1/src/main/java/UserDaoTest.java
+++ chapter1/src/main/java/UserDaoTest.java
@@ -1,5 +1,5 @@
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.*;
 import springbook.user.domain.User;
 
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
